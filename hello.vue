499f1001717e3365933d1a21679d8e5c2a1094a1
li.wei <>
2020-11-04 22:49:10 +0800

    day01

---
+++ hello.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div v-for="item in items" :key="item.id">
+            {{ item }}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                key: value
+            }
+        },
+        methods: {
+            name() {
+                
+            }
+        },
+        computed: {
+            name() {
+                return this.data 
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
